e974fe9c0c29395481d59228045cc08e44cba400
Cory Holenstein <cholensteinwork@gmail.com>
2026-07-11 21:00:54 -0400

    dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,9 @@
-<<<<<<< HEAD
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
-import { ThemeProvider } from '@mui/material/styles'
-import CssBaseline from '@mui/material/CssBaseline'
-import AppShell from './components/AppShell'
-=======
 import { BrowserRouter, Link as RouterLink, NavLink, Route, Routes, useLocation } from 'react-router-dom'
 import { Box, Button, CssBaseline, Stack, ThemeProvider, Typography, createTheme } from '@mui/material'
->>>>>>> master
 import LandingPage from './pages/LandingPage'
 import CustomerPage from './pages/CustomerPage'
 import EnforcerPage from './pages/EnforcerPage'
 import OwnerPage from './pages/OwnerPage'
-<<<<<<< HEAD
-import { appTheme } from './app/theme'
-
-function App() {
-  return (
-    <ThemeProvider theme={appTheme}>
-      <CssBaseline />
-      <BrowserRouter>
-        <AppShell>
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/customer" element={<CustomerPage />} />
-            <Route path="/customer/:lotId" element={<CustomerPage />} />
-            <Route path="/enforcer" element={<EnforcerPage />} />
-            <Route path="/owner" element={<OwnerPage />} />
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
-        </AppShell>
-=======
 import LogoutPage from './pages/LogoutPage'
 import Callback from './pages/Callback'
 import useHydrateAuth from './hooks/useHydrateAuth'
@@ -145,10 +119,9 @@ function App() {
       <CssBaseline />
       <BrowserRouter>
         <AppRoutes />
->>>>>>> master
       </BrowserRouter>
     </ThemeProvider>
   )
 }
 
-export default App
+export default App;
